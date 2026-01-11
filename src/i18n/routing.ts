d8a419094246c6a388f1db0e8041a3564b5faf46
Gustavo Henrique <110747991+gustavoppdev@users.noreply.github.com>
2026-01-11 17:20:29 -0300

    feat(setup): setup inicial do shadcn e do next-intl (#1)

---
+++ src/i18n/routing.ts
@@ -0,0 +1,13 @@
+import { defineRouting } from "next-intl/routing";
+
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales: ["en", "pt"],
+
+  // Used when no locale matches
+  defaultLocale: "pt",
+
+  pathnames: {},
+});
+
+export type Pathnames = keyof typeof routing.pathnames;
