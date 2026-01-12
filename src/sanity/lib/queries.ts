46711372683e7af8e08a1735c9b24386852cf460
Gustavo Henrique <gustavopp.dev@gmail.com>
2026-01-12 17:27:22 -0300

    feat(tanstack): adiciona e configura tanstack ao projeto, e o integra ao sanity

---
+++ src/sanity/lib/queries.ts
@@ -0,0 +1,21 @@
+import { defineQuery } from "next-sanity";
+import { client } from "./client";
+
+// 1. Definimos a query usando defineQuery (isso habilita o TypeGen)
+export const GET_PRODUCTS_QUERY =
+  defineQuery(`*[_type == "product"] | order(_createdAt desc) {
+    _id,
+    name,
+    slug,
+    price,
+    category,
+    colors,
+    "imageUrl": image.asset->url
+  }`);
+
+export const getProducts = async () => {
+  // 2. Passamos a query para o fetch.
+  // Após rodar o comando 'generate', o Sanity saberá tipar o retorno automaticamente.
+  const products = await client.fetch(GET_PRODUCTS_QUERY);
+  return products;
+};
